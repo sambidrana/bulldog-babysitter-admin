ba3fdad9e4b25ef2d706e8d9d1b0e080a0250b1a
Sambid Rana <sambid.rana1@gmail.com>
2023-12-06 23:26:37 +1100

    minor updates

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,16 +1,26 @@
 import Link from "next/link";
-import React from "react";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Nav = () => {
+  const router = useRouter();
+  const inactiveLink = "flex gap-1 p-2 pr-4 pl-4 ";
+  const activeLink = `${inactiveLink} bg-gray-100 text-black  rounded-lg`;
+
   return (
     <>
       <aside className="flex flex-col m-3 mt-10 ">
         <h1>LOGO</h1>
         <nav className="flex flex-col gap-3 p-8 mt-10">
-          <Link href={"/"}>Bookings</Link>
-          <Link href={"/boarding"}>Boarding</Link>
-          <div>
+          <Link href="/" className={router.asPath === "/" ? activeLink : inactiveLink}>
+            Bookings
+          </Link>
+          <Link
+            href="/boarding"
+            className={router.asPath.startsWith("/boarding") ? activeLink : inactiveLink}
+          >
+            Boarding
+          </Link>
+          <div className={inactiveLink}>
             <button onClick={() => signOut()}>LogOut</button>
           </div>
         </nav>
--- pages/boarding/view/[...id].js
+++ pages/boarding/view/[...id].js
@@ -2,6 +2,7 @@ import Layout from "@/components/Layout";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const View = () => {
   const [memberInfo, setMemberInfo] = useState(null); // Change initial state to null
@@ -32,57 +33,73 @@ const View = () => {
         <table className="w-full mt-6 text-left rounded shadow-sm">
           <tbody>
             <tr>
-              <th className="border border-gray-200 customTH">Chip Number</th>
-              <td className="border border-gray-500">
-                {memberInfo.chipNumber}
+              <td className="pt-5 text-2xl pb-4 pl-5 font-semibold text-gray-500 bg-white">
+                Owner's Information
               </td>
             </tr>
-
-            
-            <tr>
-              <th className="border border-gray-200 customTH">Owner</th>
-              <td className="border border-gray-500">
+            <tr className="customTR">
+              <th className=" customTH">Owner</th>
+              <td className=" customTD">
                 {memberInfo.firstName} {memberInfo.lastName}
               </td>
             </tr>
-            <tr>
-              <th className="border border-gray-200 customTH">Pet Name</th>
-              <td className="border border-gray-500">{memberInfo.petName}</td>
+            <tr className="customTR">
+              <th className=" customTH">Phone</th>
+              <td className=" customTD">{memberInfo.phone}</td>
             </tr>
-            <tr>
-              <th className="border border-gray-200 customTH">Pet Age</th>
-              <td className="border border-gray-500">{memberInfo.petAge}</td>
+            <tr className="customTR">
+              <th className=" customTH">Email</th>
+              <td className=" customTD">{memberInfo.email}</td>
             </tr>
 
             <tr>
-              <th className="border border-gray-200 customTH">Pet Notes</th>
-              <td className="border border-gray-500">{memberInfo.petNotes}</td>
+              <td className="pt-10 text-2xl pb-4 pl-5 font-semibold text-gray-500 bg-white">
+                Dog's Details
+              </td>
             </tr>
-            <tr>
-              <th className="border border-gray-200 customTH">Pet Type</th>
-              <td className="border border-gray-500">{memberInfo.petType}</td>
+
+            <tr className="customTR">
+              <th className=" customTH">Pet Name</th>
+              <td className=" customTD">{memberInfo.petName}</td>
             </tr>
-            <tr>
-              <th className="border border-gray-200 customTH">Phone</th>
-              <td className="border border-gray-500">{memberInfo.phone}</td>
+            <tr className="customTR">
+              <th className=" customTH">Pet Age</th>
+              <td className=" customTD">{memberInfo.petAge}</td>
             </tr>
-            <tr>
-              <th className="border border-gray-200 customTH">Email</th>
-              <td className="border border-gray-500">{memberInfo.email}</td>
+            <tr className="customTR">
+              <th className=" customTH">Chip Number</th>
+              <td className=" customTD">{memberInfo.chipNumber}</td>
             </tr>
-            <tr>
-              <th className="border border-gray-200 customTH">Vaccines</th>
-              <td className="border border-gray-500">{memberInfo.vaccines}</td>
+            <tr className="customTR">
+              <th className=" customTH">Pet Notes</th>
+              <td className=" customTD">{memberInfo.petNotes}</td>
             </tr>
-            <tr>
-              <th className="border border-gray-200 customTH">Created At</th>
-              <td className="border border-gray-500">
+            <tr className="customTR">
+              <th className=" customTH">Pet Type</th>
+              <td className=" customTD">{memberInfo.petType}</td>
+            </tr>
+
+            <tr className="customTR">
+              <th className=" customTH">Vaccines</th>
+              <td className=" customTD">{memberInfo.vaccines}</td>
+            </tr>
+            <tr className="customTR">
+              <th className=" customTH">Registration Date and Time</th>
+              <td className=" customTD">
                 {" "}
                 {new Date(memberInfo.createdAt).toLocaleString()}
               </td>
             </tr>
           </tbody>
         </table>
+        <div className="mt-7">
+          <Link
+            href={"/boarding"}
+            className=" text-gray-400 border border-gray-400 rounded-md p-2 pl-4 pr-4 hover:border-white hover:bg-slate-400 hover:text-white"
+          >
+            Back
+          </Link>
+        </div>
       </div>
     </Layout>
   );
