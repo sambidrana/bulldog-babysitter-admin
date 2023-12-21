295b87f11fd8ef2f9c44551e6df280874f22e737
Sambid Rana <sambid.rana1@gmail.com>
2023-12-21 22:31:38 +1100

    form.parse complete

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -3,7 +3,9 @@ import Cors from "cors";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import multiparty from "multiparty";
 import fs from "fs";
+import http from "node:http";
 
+import formidable from "formidable";
 const bucketName = "bulldog-babysitter-uploads"; // Ensure this is your bucket name
 
 const cors = Cors({
@@ -32,60 +34,88 @@ function runMiddleware(req, res, fn) {
 }
 
 export default async function handleUpload(req, res) {
+  await mongooseConnect();
   await runMiddleware(req, res, cors);
-  const { method } = req;
 
-  if (method === "POST") {
-    const form = new multiparty.Form({
-      maxFileSize: 10 * 1024 * 1024, // Adjust as necessary
-    });
+  if (req.method === "POST") {
+    const form = new formidable.IncomingForm();
 
-    form.parse(req, function (err, fields, files) {
+    form.parse(req, (err, fields, files) => {
       if (err) {
         console.error("Error parsing form:", err);
-        return res.status(500).send("Error parsing the form data");
+        return res.status(500).json({ error: "Error parsing the form data" });
       }
 
       console.log("Fields:", fields);
       console.log("Files:", files);
 
-      res.status(200).json({ message: "Form parsed successfully" });
+      return res.status(200).json({ message: "Success" });
     });
   } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-
-    // const formData = await req;
-    // console.log(formData);
-    // return res.json('ok')
+    res.status(405).json({ error: "Method Not Allowed" });
   }
 
-  //   if (method === "POST") {
-  //     const form = new multiparty.Form();
-
-  //     form.parse(req, (err, fields, files) => {
-  //       // Check for errors during parsing
-  //       if (err) {
-  //         console.error("Error parsing form: ", err);
-  //         return res.status(500).send("Error parsing the form data");
-  //       }
-
-  //       // Log the received fields and files
-  //       console.log("Fields:", fields);
-  //       console.log("Files:", files);
-
-  //       // At this point, you should handle the files as needed.
-  //       // For example, you could upload them to S3, save info to a database, etc.
-  //       // Since this is just logging, we'll send a response back to the client.
-
-  //       res.status(200).send("Form data received");
-  //     });
-  //   } else {
-  //     // Handle non-POST requests
-  //     res.setHeader("Allow", ["POST"]);
-  //     res.status(405).end(`Method ${req.method} Not Allowed`);
-  //   }
+  //   console.log(req)
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+// const form = new multiparty.Form();
+// const { fields, files } = await new Promise((resolve, reject) => {
+//     form.parse(req, (err, fields, files) => {
+//       if (err) reject(err);
+//       resolve({ fields, files });
+//     });
+//   });
+
+// const form = new multiparty.Form({
+//     maxFileSize: 5 * 1024 * 1024, // 5MB for example
+//   });
+//   form.parse(req, function (err, fields, files) {
+//     if (err) {
+//       console.error("Error parsing form:", err);
+//       return res.status(500).send("Error parsing the form data");
+//     }
+
+//     console.log("Fields:", fields);
+//     console.log("Files:", files);
+
+//     res.status(200).json({ message: "Form parsed successfully" });
+//   });
+
+// const formData = await req;
+// console.log(formData);
+// return res.json('ok')
+
+//   if (method === "POST") {
+//     const form = new multiparty.Form();
+
+//     form.parse(req, (err, fields, files) => {
+//       // Check for errors during parsing
+//       if (err) {
+//         console.error("Error parsing form: ", err);
+//         return res.status(500).send("Error parsing the form data");
+//       }
+
+//       // Log the received fields and files
+//       console.log("Fields:", fields);
+//       console.log("Files:", files);
+
+//       // At this point, you should handle the files as needed.
+//       // For example, you could upload them to S3, save info to a database, etc.
+//       // Since this is just logging, we'll send a response back to the client.
+
+//       res.status(200).send("Form data received");
+//     });
+//   } else {
+//     // Handle non-POST requests
+//     res.setHeader("Allow", ["POST"]);
+//     res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
 
 // if (req.method === "POST") {
 
